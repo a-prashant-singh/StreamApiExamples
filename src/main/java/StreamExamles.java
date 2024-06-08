8d6910ccd366557b8900b4e0d4ad96b477c88d1a
parshya <prashsingh1997@gmail.com>
2024-06-08 14:11:43 +0530

    Transformed each element in double

--- src/main/java/StreamExamles.java
+++ src/main/java/StreamExamles.java
@@ -1,15 +1,26 @@
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamExamles {
 
 
     public static void main(String[] args) {
-        List<Integer> list= Arrays.asList(4,6,1,34,23,1,33,2345,675,5);
+        List<Integer> list= Arrays.asList(3,6,1,34,23,1,33,234,675,505);
 
         Stream<Integer> stream=list.stream();
+
         stream.forEach(System.out::println);
+
+        //UC 2.2
+        List<Double> doubleList=list.stream().map(Integer->Integer.doubleValue()).toList();
+        //or
+        //List<Double> doubleList=list.stream().map(Integer::doubleValue).toList();
+
+
+
     }
 }
