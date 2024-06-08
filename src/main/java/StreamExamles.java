4c299bc0fbaf9fb41196dec078bfc9d29c83a563
parshya <prashsingh1997@gmail.com>
2024-06-08 14:20:16 +0530

    Stored transfomed double list in new list

--- src/main/java/StreamExamles.java
+++ src/main/java/StreamExamles.java
@@ -10,6 +10,18 @@ public class StreamExamles {
         List<Integer> list= Arrays.asList(4,6,1,34,23,1,33,2345,675,5);
 
         Stream<Integer> stream=list.stream();
+
         stream.forEach(System.out::println);
+
+        //UC 2.2
+        list.stream().map(Integer->Integer.doubleValue()).toList();
+        //or
+        //list.stream().map(Integer::doubleValue).toList();
+
+        //UC 2.3
+        List<Double> douIntegerList= list.stream().map(Integer->Integer.doubleValue()).toList();
+        //or
+        // List<Double> douIntegerList=list.stream().map(Integer::doubleValue).toList();
+
     }
 }
