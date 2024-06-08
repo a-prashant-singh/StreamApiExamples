e4bf5ab737fc0b60de1a5feb80d124ed9798e44d
parshya <prashsingh1997@gmail.com>
2024-06-08 14:24:34 +0530

    Find first even number

--- src/main/java/StreamExamles.java
+++ src/main/java/StreamExamles.java
@@ -10,6 +10,23 @@ public class StreamExamles {
         List<Integer> list= Arrays.asList(4,6,1,34,23,1,33,2345,675,5);
 
         Stream<Integer> stream=list.stream();
+
         stream.forEach(System.out::println);
+
+        //UC 2.2
+        list.stream().map(Integer->Integer.doubleValue()).toList();
+        //or
+        //list.stream().map(Integer::doubleValue).toList();
+
+        //UC 2.3
+        List<Double> douIntegerList= list.stream().map(Integer->Integer.doubleValue()).toList();
+        //or
+        // List<Double> douIntegerList=list.stream().map(Integer::doubleValue).toList();
+
+        //UC 2.4
+        int firstEvenNumber=list.stream().filter(n->n%2==0).peek(n-> System.out.println("Peak even number: "+n)).findFirst().orElseThrow();
+        //System.out.println(firstEvenNumber);
+
+
     }
 }
